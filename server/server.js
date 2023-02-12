5754983089920461a4c6475fb93cb2ab1cebd559
Michael Nabatov <happyanddebtfree@gmail.com>
2023-02-12 10:45:39 -0500

    Https Redirect for Node on Heroku

--- server/server.js
+++ server/server.js
@@ -22,8 +22,14 @@ app.use(express.json());
 // app.use('/images', express.static(path.join(__dirname, '../client/images')));
 
 if (process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https')
+      res.redirect(`https://${req.header('host')}${req.url}`);
+    else
+      next();
+  });
   app.use(express.static(path.join(__dirname, '../client/build')));
-}
+};
 
 app.get('/', (req, res) => {
   console.log('get /');
