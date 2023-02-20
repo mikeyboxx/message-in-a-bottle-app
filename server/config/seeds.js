8a526bb1a0097f8c86b444e8d18cb748c0eee205
Michael Nabatov <happyanddebtfree@gmail.com>
2023-02-20 14:14:01 -0500

    optimized code

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -3,9 +3,9 @@ const { Note } = require('../models');
 const { circleXY, getLatLonGivenDistanceAndBearing } = require('../utils/generateRandomMarkers');
 
 db.once('open', async () => {
-  const distanceInMeters = 25000; // 5 miles
-  const lat = 40.5736681;
-  const lng = -74.0055502;
+  const distanceInMeters = 8046; // 5 miles
+  let lat = 40.5736681;
+  let lng = -74.0055502;
 
   await Note.deleteMany();
 
@@ -14,7 +14,20 @@ db.once('open', async () => {
     const {x, y} = circleXY(randomDistance, theta);
     const position = getLatLonGivenDistanceAndBearing(lat, lng, x, y );
     const note = await Note.create({
-      noteText: `This is Note #: ${theta + 1}`,
+      noteText: `This is Note #: ${(theta + 1).toFixed(1)}`,
+      lat: position.lat,
+      lng: position.lng,
+    });
+  }
+
+  lat = 34.1430296530324;
+  lng = -118.09969814240472;
+  for (let theta=0; theta<360; theta += .1) {
+    const randomDistance = Math.floor(Math.random() * distanceInMeters) + 10;
+    const {x, y} = circleXY(randomDistance, theta);
+    const position = getLatLonGivenDistanceAndBearing(lat, lng, x, y );
+    const note = await Note.create({
+      noteText: `This is Note #: ${(theta + 1).toFixed(1)}`,
       lat: position.lat,
       lng: position.lng,
     });
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -15,6 +15,7 @@ const typeDefs = gql`
   type NoteInBounds {
     note: Note
     distance: Float
+    inProximity: Boolean
   }
 
   type Query {
