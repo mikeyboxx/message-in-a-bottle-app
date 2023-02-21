cd9ba9881d947a1344f185985c99675f2baf5091
Michael Nabatov <happyanddebtfree@gmail.com>
2023-02-21 14:31:35 -0500

    trying to fix bug with viewport layout in google chrome

--- client/src/components/MapContainer/index.js
+++ client/src/components/MapContainer/index.js
@@ -10,7 +10,9 @@ import { QUERY_NOTES_IN_BOUNDS } from '../../utils/queries';
 export default function MapContainer({startingPosition}) {
   const [position, setPosition] = useState(null);
   const [notesInBounds, setNotesInBounds] = useState(null);
-  const [getNotesInBounds, {data}] = useLazyQuery(QUERY_NOTES_IN_BOUNDS);
+  const [getNotesInBounds, {data}] = useLazyQuery(QUERY_NOTES_IN_BOUNDS,{
+    fetchPolicy: 'network-only'
+  });
   
   const map = useRef(null);
   const prevPosition = useRef({});
@@ -36,21 +38,7 @@ export default function MapContainer({startingPosition}) {
     path: "M160.8 96.5c14 17 31 30.9 49.5 42.2c25.9 15.8 53.7 25.9 77.7 31.6V138.8C265.8 108.5 250 71.5 248.6 28c-.4-11.3-7.5-21.5-18.4-24.4c-7.6-2-15.8-.2-21 5.8c-13.3 15.4-32.7 44.6-48.4 87.2zM320 144v30.6l0 0v1.3l0 0 0 32.1c-60.8-5.1-185-43.8-219.3-157.2C97.4 40 87.9 32 76.6 32c-7.9 0-15.3 3.9-18.8 11C46.8 65.9 32 112.1 32 176c0 116.9 80.1 180.5 118.4 202.8L11.8 416.6C6.7 418 2.6 421.8 .9 426.8s-.8 10.6 2.3 14.8C21.7 466.2 77.3 512 160 512c3.6 0 7.2-1.2 10-3.5L245.6 448H320c88.4 0 160-71.6 160-160V128l29.9-44.9c1.3-2 2.1-4.4 2.1-6.8c0-6.8-5.5-12.3-12.3-12.3H400c-44.2 0-80 35.8-80 80zm80 16c-8.8 0-16-7.2-16-16s7.2-16 16-16s16 7.2 16 16s-7.2 16-16 16z",
   }),[]);
 
-  // default GoogleMap options
-  const defaultMapOptions = useMemo(()=>({ 
-    disableDefaultUI: true,
-    minZoom: 16,
-    mapId: '8dce6158aa71a36a',
-    maxZoom: 20,
-    // isFractionalZoomEnabled: false,
-  }),[]);
-
-  // default GoogleMap styles
-  // const mapContainerStyle = useMemo(()=>({ 
-  //   // height: '700px', 
-  //   height: `${window.innerHeight}px`, 
-  //   width: '100%',
-  // }),[]);
+  
   
   // track google map events
   const onDragEnd = useCallback(() => {/*console.log('onDragEnd');*/ return dragEnd.current = true},[]);
@@ -177,9 +165,13 @@ export default function MapContainer({startingPosition}) {
       {position && 
         <GoogleMap
           id={'googleMap'}
-          options={defaultMapOptions}
+          options={{ 
+            disableDefaultUI: true,
+            minZoom: 16,
+            mapId: '8dce6158aa71a36a',
+            maxZoom: 20,
+          }}
           mapContainerStyle={{ 
-            // minHeight: '50', 
             height: `${window.screen.height >= window.innerHeight ? 
                        window.innerHeight : 
                        window.screen.height - (window.innerHeight - window.screen.height)}px`, 
@@ -215,55 +207,54 @@ export default function MapContainer({startingPosition}) {
           })}
 
           {map.current && notesInBounds && 
-        <Button 
-        size="lg" 
-        variant="info"
-        style={{
-          position: 'absolute',
-          border: 'none',
-          borderRadius: 30,
-          boxShadow: '5px 5px 5px gray',
-          bottom: 30,
-          left: 20,
-          paddingTop: 8,
-          paddingBottom: 8,
-          paddingLeft: 18,
-          paddingRight: 18,
-          fontWeight: 'bold',
-          backgroundColor: 'white',
-          color: 'purple',
-          fontSize: '.85em',
-          cursor: 'pointer'
-        }}
-      >
-        <Journals /> Create Note
-      </Button>}
+            <Button 
+                size="lg" 
+                variant="info"
+                style={{
+                  position: 'absolute',
+                  border: 'none',
+                  borderRadius: 30,
+                  boxShadow: '5px 5px 5px gray',
+                  bottom: 30,
+                  left: 20,
+                  paddingTop: 8,
+                  paddingBottom: 8,
+                  paddingLeft: 18,
+                  paddingRight: 18,
+                  fontWeight: 'bold',
+                  backgroundColor: 'white',
+                  color: 'purple',
+                  fontSize: '.85em',
+                  cursor: 'pointer'
+                }}
+              >
+                <Journals /> Create Note
+            </Button>}
       
-      {map.current && notesInBounds &&
-        notesInBounds?.filter(note => note.inProximity === true).length > 0 && 
-        <Button 
-          size="lg" 
-          variant="info"
-          style={{
-            position: 'absolute',
-            border: 'none',
-            borderRadius: 30,
-            boxShadow: '5px 5px 5px gray',
-            bottom: 30,
-            left: 210,
-            paddingTop: 8,
-            paddingBottom: 8,
-            paddingLeft: 18,
-            paddingRight: 18,
-            fontWeight: 'bold',
-            backgroundColor: 'white',
-            color: 'purple',
-            fontSize: '.85em',
-            cursor: 'pointer'
-          }}
-        >
-          <Journals /> Pickup {numberOfNotesInProximity.current + ' Note' + (numberOfNotesInProximity.current > 1 && 's')}
-        </Button>}
+            {map.current && notesInBounds?.filter(note => note.inProximity === true).length > 0 && 
+              <Button 
+                size="lg" 
+                variant="info"
+                style={{
+                  position: 'absolute',
+                  border: 'none',
+                  borderRadius: 30,
+                  boxShadow: '5px 5px 5px gray',
+                  bottom: 30,
+                  left: 210,
+                  paddingTop: 8,
+                  paddingBottom: 8,
+                  paddingLeft: 18,
+                  paddingRight: 18,
+                  fontWeight: 'bold',
+                  backgroundColor: 'white',
+                  color: 'purple',
+                  fontSize: '.85em',
+                  cursor: 'pointer'
+                }}
+              >
+                <Journals /> Pickup {numberOfNotesInProximity.current + ' Note' + (numberOfNotesInProximity.current > 1 ? 's' : '')}
+              </Button>}
         </GoogleMap>}
 
       
@@ -287,16 +278,16 @@ export default function MapContainer({startingPosition}) {
             
           <p>
             {/* DevicePixelRatio: {window.devicePixelRatio} <br/> */}
-            googleMap height: {document.getElementById('googleMap').getBoundingClientRect().height
+            {/* googleMap height: {document.getElementById('googleMap').getBoundingClientRect().height
 }<br/>
             googleMap width: {document.getElementById('googleMap').getBoundingClientRect().width
-}<br/><br/>
+}<br/><br/> */}
             {/* VisualViewport height: {window.visualViewport.innerHeight} <br/> 
             VisualViewport width: {window.visualViewport.innerWidth} <br/> 
             VisualViewport offsetTop: {window.visualViewport.offsetTop} <br/> 
             VisualViewport offsetLeft: {window.visualViewport.offsetLeft} <br/> 
             VisualViewport scale: {window.visualViewport.scale} <br/>  */}
-            Inner height: {window.innerHeight} <br/> 
+            {/* Inner height: {window.innerHeight} <br/> 
             Inner width: {window.innerWidth} <br/> <br/>
             Outer height: {window.outerHeight} <br/> 
             Outer width: {window.outerWidth} <br/> <br/>
@@ -305,7 +296,7 @@ export default function MapContainer({startingPosition}) {
             Available height: {window.screen.availHeight} <br/>
             Available width: {window.screen.availWidth} <br/> <br/>
             Body client height: {document.querySelector('body').clientHeight} <br/>
-            Body client width: {document.querySelector('body').clientWidth} <br/><br/>
+            Body client width: {document.querySelector('body').clientWidth} <br/><br/> */}
             Zoom: {map.current.zoom} <br/> <br/>
             Distance travelled: {window.google.maps.geometry.spherical.computeDistanceBetween(
               {lat: prevPosition.current.lat || 0, lng: prevPosition.current.lng || 0},
