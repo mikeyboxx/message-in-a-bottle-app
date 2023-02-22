f9ff4a22a040c4ee83d0471c486e968148c745a3
Michael Nabatov <happyanddebtfree@gmail.com>
2023-02-22 17:40:01 -0500

    optimized code

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,25 @@
 import {useState, useEffect} from 'react';
 import {useJsApiLoader} from '@react-google-maps/api';
-
-
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from '@apollo/client';
+import {ApolloClient, InMemoryCache, ApolloProvider, createHttpLink,} from '@apollo/client';
 // import { setContext } from '@apollo/client/link/context';
 import MapContainer from './components/MapContainer';
 // import {getLatLonBounds} from './utils/trigonometry';
 
+// const authLink = setContext((_, { headers }) => {
+//   const token = localStorage.getItem('id_token');
+//   return {
+//     headers: {
+//       ...headers,
+//       authorization: token ? `Bearer ${token}` : '',
+//     },
+//   };
+// });
 
 // used by google maps api param
-const libraries = ['geometry'];
-
-const httpLink = createHttpLink({
-  uri: '/graphql',
-});
-
-// const authLink = setContext((_, { headers }) => {
-  //   const token = localStorage.getItem('id_token');
-  //   return {
-    //     headers: {
-      //       ...headers,
-      //       authorization: token ? `Bearer ${token}` : '',
-      //     },
-      //   };
-      // });
-
-      // connect to graphQL on the server
+const googleLibraries = ['geometry'];
+const httpLink = createHttpLink({uri: '/graphql'});
 const client = new ApolloClient({
-  // link: authLink.concat(httpLink),
+// link: authLink.concat(httpLink),
   link: httpLink,
   cache: new InMemoryCache(),
 });
@@ -42,7 +29,7 @@ function App() {
   const [startingPosition, setStartingPosition] = useState(null);
   const {isLoaded, loadError} = useJsApiLoader({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-    libraries: libraries
+    libraries: googleLibraries
   });
 
   useEffect(()=>{
