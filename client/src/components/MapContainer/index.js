dd7650c45a585ad99f96a78cee7010ccbf17a305
Michael Nabatov <happyanddebtfree@gmail.com>
2023-02-22 17:40:01 -0500

    optimized code

--- client/src/components/MapContainer/index.js
+++ client/src/components/MapContainer/index.js
@@ -4,7 +4,6 @@ import {Journals} from 'react-bootstrap-icons';
 import {GoogleMap, Marker} from '@react-google-maps/api';
 import { useLazyQuery } from '@apollo/client';
 import { QUERY_NOTES_IN_BOUNDS } from '../../utils/queries';
-// import noteImg from '../../img/note.png';
 
 
 export default function MapContainer({startingPosition}) {
@@ -38,8 +37,6 @@ export default function MapContainer({startingPosition}) {
     path: "M160.8 96.5c14 17 31 30.9 49.5 42.2c25.9 15.8 53.7 25.9 77.7 31.6V138.8C265.8 108.5 250 71.5 248.6 28c-.4-11.3-7.5-21.5-18.4-24.4c-7.6-2-15.8-.2-21 5.8c-13.3 15.4-32.7 44.6-48.4 87.2zM320 144v30.6l0 0v1.3l0 0 0 32.1c-60.8-5.1-185-43.8-219.3-157.2C97.4 40 87.9 32 76.6 32c-7.9 0-15.3 3.9-18.8 11C46.8 65.9 32 112.1 32 176c0 116.9 80.1 180.5 118.4 202.8L11.8 416.6C6.7 418 2.6 421.8 .9 426.8s-.8 10.6 2.3 14.8C21.7 466.2 77.3 512 160 512c3.6 0 7.2-1.2 10-3.5L245.6 448H320c88.4 0 160-71.6 160-160V128l29.9-44.9c1.3-2 2.1-4.4 2.1-6.8c0-6.8-5.5-12.3-12.3-12.3H400c-44.2 0-80 35.8-80 80zm80 16c-8.8 0-16-7.2-16-16s7.2-16 16-16s16 7.2 16 16s-7.2 16-16 16z",
   }),[]);
 
-  
-  
   // track google map events
   const onDragEnd = useCallback(() => {/*console.log('onDragEnd');*/ return dragEnd.current = true},[]);
   const onZoomChanged = useCallback(() => {/*console.log('onZoomChanged');*/ return zoomChanged.current = true},[]);
@@ -63,8 +60,8 @@ export default function MapContainer({startingPosition}) {
   const onIdle = useCallback(() => {
     // console.log('onIdle');
     if (map.current && 
-      (zoomChanged.current === true || dragEnd.current === true || boundsChanged.current === true)){
-        if (map.current.zoom > 15) {
+      (zoomChanged.current || dragEnd.current || (boundsChanged.current  && prevPosition.current.isChanged))){
+        if (map.current.zoom > 17) {
           const newBounds = map.current.getBounds();
           if (newBounds) {
             const isInBounds = 
@@ -73,10 +70,8 @@ export default function MapContainer({startingPosition}) {
                 position.coords.latitude < newBounds.getNorthEast().lat() && 
                 position.coords.longitude <  newBounds.getNorthEast().lng();
                 
-            ((!isInBounds && (zoomChanged.current || dragEnd.current)) ||  boundsChanged.current) && 
+            ((!isInBounds && (zoomChanged.current || dragEnd.current)) || boundsChanged.current) && 
               getNotesInBounds({variables: {
-                // lat: isInBounds ? position.coords.latitude : null,
-                // lng: isInBounds ? position.coords.longitude : null,
                 swLat: newBounds.getSouthWest().lat(), 
                 swLng: newBounds.getSouthWest().lng(), 
                 neLat: newBounds.getNorthEast().lat(), 
@@ -87,6 +82,8 @@ export default function MapContainer({startingPosition}) {
           setNotesInBounds([]);
         }
     }
+    if (prevPosition.current.isChanged) prevPosition.current.isChanged = false;
+
     zoomChanged.current = false;
     dragEnd.current = false;
     boundsChanged.current = false;
@@ -123,6 +120,7 @@ export default function MapContainer({startingPosition}) {
     if (position) {
       // first time logic
       if (Object.keys(prevPosition.current).length === 0){
+        prevPosition.current.isChanged = true;
         prevPosition.current.lat = position.coords.latitude;
         prevPosition.current.lng = position.coords.longitude;
         
@@ -144,6 +142,7 @@ export default function MapContainer({startingPosition}) {
           if (dist > 100) {
             prevPosition.current.lat = position.coords.latitude;
             prevPosition.current.lng = position.coords.longitude;
+            prevPosition.current.isChanged = true;
             // isInBounds && map.current.zoom > 15 && getNotesInBounds({variables: {
             //   lat: position.coords.latitude,
             //   lng: position.coords.longitude,
@@ -154,8 +153,8 @@ export default function MapContainer({startingPosition}) {
             // }});
           }
           
-          dist > 3 && isInBounds && map.current.panTo({lat: position.coords.latitude, lng: position.coords.longitude})
-          dist > 3 && isInBounds && position.coords.accuracy < 13 && position.coords.speed > .5 && map.current.setHeading(position.coords.heading);
+          isInBounds && map.current.panTo({lat: position.coords.latitude, lng: position.coords.longitude})
+          isInBounds && position.coords.accuracy < 13 && position.coords.speed > .5 && map.current.setHeading(position.coords.heading);
         }
       }
     }
@@ -168,7 +167,7 @@ export default function MapContainer({startingPosition}) {
     // if (data?.notesInBounds) {
     //   setNotesInBounds(data?.notesInBounds);
     // }
-    if (data?.notesInBounds && position ) {
+    if (data?.notesInBounds && position && map.current.zoom > 17) {
       const arr = data.notesInBounds.map(el => {
         const { note } = el;
         const distance =  window.google.maps.geometry.spherical.computeDistanceBetween(
@@ -197,7 +196,7 @@ export default function MapContainer({startingPosition}) {
             disableDefaultUI: true,
             // minZoom: 16,
             mapId: '8dce6158aa71a36a',
-            isFractionalZoomEnabled: false,
+            // isFractionalZoomEnabled: false,
             // maxZoom: 20,
           }}
           mapContainerStyle={{ 
