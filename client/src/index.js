8c2e556057dc03e08fec12a02692200c8451d7e1
Michael Nabatov <happyanddebtfree@gmail.com>
2023-02-24 19:54:53 -0500

    trying to fix the height issue aghain

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 window.addEventListener('resize', () =>{ 
-  // console.log(window.screen.height, window.innerHeight);
-  // console.log(window.screen.width , window.innerWidth);
+  console.log('before');
+  console.log(window.screen.height, window.innerHeight, window.clientHeight);
+  console.log(window.screen.width , window.innerWidth, window.clientHeight);
   
   setTimeout(()=>{
-  //   console.log(window.screen.height, window.innerHeight);
-  //   console.log(window.screen.width , window.innerWidth);
-  // }, 0);
+    console.log('after');
+    console.log(window.screen.height, window.innerHeight, window.clientHeight);
+    console.log(window.screen.width , window.innerWidth, window.clientHeight);
+  
 
   },0); 
 });
