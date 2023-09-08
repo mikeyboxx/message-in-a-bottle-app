47bb884eccf0dbad42176d17385adf367e243afe
Michael Nabatov <happyanddebtfree@gmail.com>
2023-09-08 14:38:20 -0400

    Changed the seed process to be more modular

--- server/utils/seedDaemon.js
+++ server/utils/seedDaemon.js
@@ -3,55 +3,31 @@ const { Note, User } = require('../models');
 const { circleXY, getLatLonGivenDistanceAndBearing } = require('./trigonometry');
 const getRandomQuote = require('./getRandomQuote');
 
-module.exports = async () => {
-  console.log('seedDaemon started on: ', new Date.toLocaleString());
+module.exports = async ({distanceInMeters, userTargetArr, timeIntervalMilliSecs, timeIntervalMilliSecsLimit}) => {
+  console.log('seedDaemon started on: ', new Date().toLocaleString());
 
-  const distanceInMeters = 8046; // 5 miles
-
-  const userTargetArr = [
-    {
-      lat: 40.5736681,
-      lng: -74.0055502
-    },
-    {
-      lat: 40.639240,
-      lng: -74.001620
-    },
-    // {
-    //   lat: 34.1430296,
-    //   lng: -118.0996981
-    // },
-    // {
-    //   lat: 33.616935,
-    //   lng: -78.999047
-    // },
-  ];
-  
-  const ms = 5000;
   let ctr = 0;
 
   const timer = setInterval(async () => {
-    ctr = ctr + (ms / 1000);
-    // console.log('seedDaemon', ctr);
+    ctr = ctr + timeIntervalMilliSecs;
 
-    // if (ctr > 86400) {
-    if (ctr > 3600) {
-      console.log('seedDaemon finished on: ', new Date.toLocaleString());
+    if (ctr > timeIntervalMilliSecsLimit) {
+      console.log('seedDaemon finished on: ', new Date().toLocaleString());
       clearInterval(timer); 
       process.exit(0);
-      return null;
     }
 
     for (let i = 0; i < userTargetArr.length; i++){
       try {
-          const randomDistance = Math.floor(Math.random() * distanceInMeters) + 10;
-          const randomTheta = (Math.floor(Math.random() * 3600)/10).toFixed(1);
-          const {x, y} = circleXY(randomDistance, randomTheta);
-          
-          let {lat, lng} = userTargetArr[i];
-          let position = getLatLonGivenDistanceAndBearing(lat, lng, x, y );
-          let {q, a} = await getRandomQuote();
-
+        const randomDistance = Math.floor(Math.random() * distanceInMeters) + 10;
+        const randomTheta = (Math.floor(Math.random() * 3600)/10).toFixed(1);
+        const {x, y} = circleXY(randomDistance, randomTheta);
+        
+        let {lat, lng} = userTargetArr[i];
+        let position = getLatLonGivenDistanceAndBearing(lat, lng, x, y );
+        let {q, a} = await getRandomQuote();
+        
+        if (a !== 'zenquotes.io') {
           const names = a.split(' ');
           const firstName =  names[0];
           const lastName = names.length === 1 ? '' : names.length >= 3 ? names[2] : names[1];
@@ -68,33 +44,33 @@ module.exports = async () => {
               userName,
               password: 'password12345'
             });
-            // console.log('created user ', user);
-          }
+          } else console.log(`${userName} already exists`);
 
-          const note = await Note.create({
-            noteText: q,
-            noteAuthor: userName,
-            lat: position.lat,
-            lng: position.lng,
-            // bearing: Math.floor(Math.random() * 360) + 1
-          });
+          let note = await Note.findOne({ noteText: q });
+          
+          if (!note) {
+            note = await Note.create({
+              noteText: q,
+              noteAuthor: userName,
+              lat: position.lat,
+              lng: position.lng,
+              bearing: (ctr / timeIntervalMilliSecs) % 2 === 0 ? Math.floor(Math.random() * 360) + 1 : null
+            });
 
-          // console.log('created note ', note);
-
-          const result = await User.findOneAndUpdate(
-            { _id: user._id },
-            { $addToSet: { createdNotes: note._id } },
-            { new: true }
-          );
-
-          // console.log('updated user ', result.id);
-
+            const result = await User.findOneAndUpdate(
+              { _id: user._id },
+              { $addToSet: { createdNotes: note._id } },
+              { new: true }
+            );
+          } else console.log(`${q} already exists`);
+        } else console.log(q);
       } catch (err) {
+        clearInterval(timer); 
         console.log(err);
         process.exit(1);
       }
     }
-  },ms);
+  },timeIntervalMilliSecs);
 };
 
 
