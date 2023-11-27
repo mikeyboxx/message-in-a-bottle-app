0f26d226f967f588c0616d2a726dd3c7e1447533
Michael Nabatov <happyanddebtfree@gmail.com>
2023-11-27 12:51:55 -0500

    Changed seedD script to pass it latitude and longitude as arguments in CLI

--- server/config/seedD.js
+++ server/config/seedD.js
@@ -1,23 +1,31 @@
 const db = require('./connection');
 const seedDaemon = require('../utils/seedDaemon');
 
+// console.log(process.argv);
+
 db.once('open', async () => {
+  const userTargetArr = [
+    { // oceana
+      lat: 40.576804,
+      lng: -73.956923
+    },
+    { // holywood, fl
+      lat: 26.018120,
+      lng: -80.186987
+    },
+  ];
+
+  if (process.argv.length === 4) 
+    userTargetArr.push({
+      lat: Number(process.argv[2]) || 40.573546, 
+      lng: Number(process.argv[3]) || -74.005546
+    });
+
+    // console.log(userTargetArr);
+
   seedDaemon({
     distanceInMeters: 5046,
-    userTargetArr: [
-      { // h
-        lat: 40.573546,
-        lng: -74.005546
-      },
-      { // oceana
-        lat: 40.576804,
-        lng: -73.956923
-      },
-      { // holywood, fl
-        lat: 26.018120,
-        lng: -80.186987
-      },
-    ], 
+    userTargetArr,
     timeIntervalMilliSecs: 10000,  
     timeIntervalMilliSecsLimit: 600000
   });
